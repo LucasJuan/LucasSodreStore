20dba6198e8f3208c3fe54354460278709861eab
Lucas Juan <ljgsodre@outlook.com>
2022-01-05 22:49:15 -0300

    Criando Models, Views e Controllers de login.

--- src/web/LSS.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/LSS.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LSS.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,53 @@ namespace LSS.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
-        
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            //API - Registro
+
+            if (false) return View(usuarioRegistro);
+
+            // Realizar Login na API
+
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            //API - Login
+
+            if (false) return View(usuarioLogin);
+
+            // Realizar Login na API
+
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
+        [Route("logout")]
+        public async Task<IActionResult> LogOut()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
